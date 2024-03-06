64d5dd6d8c538f950f7f286723c30159a61b49f2
AnthoNoel <anthony.noel@oclock.school>
2024-03-06 09:09:17 +0100

    fix

--- Snake/js/snake.js
+++ Snake/js/snake.js
@@ -4,16 +4,17 @@ const scoreMaxElement = document.querySelector(".high-score");
 const controls = document.querySelectorAll(".controls i");
 const up = document.querySelector(".up i")
 
+
 let gameOver = false;
 
 let foodX, foodY;
-let snakeX = 5 , snakeY = 10;
+let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let positionX = 0, positionY = 0;
 let setIntervalId;
 let score = 0;
 const keyCodes = [
-    37,38,39,40
+    37, 38, 39, 40
 ]
 
 // Getting high score from the storage local
@@ -22,8 +23,8 @@ scoreMaxElement.innerText = `High Score: ${scoreMax}`;
 
 const changeFoodPosition = () => {
     // Random food position generate 
-    foodX = Math.floor(Math.random() *30 ) + 1;
-    foodY = Math.floor(Math.random() *30 ) + 1;
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
 }
 
 const handleGameOver = () => {
@@ -31,44 +32,46 @@ const handleGameOver = () => {
     clearInterval(setIntervalId);
     alert('Game OVER! Clique sur OK pour réessayer')
     location.reload();
-    
+
 }
 const changeDirectionSnake = (e) => {
     // prevent to scroll, just activate for keyboard arrow
-    if (e.keyCode && keyCodes.indexOf(e.keyCode) !== -1){
+    if (e.keyCode && keyCodes.indexOf(e.keyCode) !== -1) {
         e.preventDefault();
     }
+
     // Direction for keyboard
-    if (e.key === 'ArrowUp' && positionX != 1){
+    if (e.key === 'ArrowUp' && positionX != 1) {
         positionX = -1;
         positionY = 0;
-    } else if(e.key === 'ArrowDown' && positionX != -1){
+    } else if (e.key === 'ArrowDown' && positionX != -1) {
         positionX = 1;
         positionY = 0;
-    } else if(e.key === 'ArrowLeft' && positionY != 1){
+    } else if (e.key === 'ArrowLeft' && positionY != 1) {
         positionX = 0;
         positionY = -1;
-    } else if(e.key === 'ArrowRight' && positionY != -1){
+    } else if (e.key === 'ArrowRight' && positionY != -1) {
         positionX = 0;
         positionY = 1;
     }
-    
+
 }
 
 controls.forEach(key => {
-    key.addEventListener('click', () => changeDirectionSnake({key: key.dataset.key}))
+    key.addEventListener('click', () => changeDirectionSnake({ key: key.dataset.key }))
 })
-up.addEventListener('click', () => changeDirectionSnake({key: up.dataset.key}));
+up.addEventListener('click', () => changeDirectionSnake({ key: up.dataset.key }));
+
 
 const initGame = () => {
 
-    
+
     if (gameOver) return handleGameOver();
     let classHtml = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
-    
+
     // Check if the snake hit food
-    if(snakeX === foodY && snakeY === foodX){
-       
+    if (snakeX === foodY && snakeY === foodX) {
+        document.documentElement.style.overflow = 'hidden';
         changeFoodPosition();
         snakeBody.push([foodX, foodY])
         score++; // increment score +1
@@ -79,12 +82,12 @@ const initGame = () => {
         scoreMaxElement.innerText = `High Score: ${scoreMax}`;
     }
 
-    for (let i = snakeBody.length -1 ; i > 0; i--) {
+    for (let i = snakeBody.length - 1; i > 0; i--) {
         // shifting forward the values of the elements in the snake body by one
-        snakeBody[i] = snakeBody[i-1];
-        
+        snakeBody[i] = snakeBody[i - 1];
+
     }
-    
+
     snakeBody[0] = [snakeY, snakeX]; // setting first element of snakebody to current position 
 
     // applicate position to snake
@@ -92,14 +95,14 @@ const initGame = () => {
     snakeY += positionY;
 
     //  checking if the snake going out of wall
-    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         gameOver = true;
     }
     for (let i = 0; i < snakeBody.length; i++) {
         // Add a new div for each part of the bodysnake
         classHtml += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
         // checking if the snake head hit his body, if so set gameover
-        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
             gameOver = true;
         }
     }
@@ -109,8 +112,6 @@ const initGame = () => {
 
     playBoard.innerHTML = classHtml;
 }
-
-
 
 
 changeFoodPosition();
